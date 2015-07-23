affcb6d49e57c64437efa51d9d0b5add87a49b28
Leomund <>
2015-07-24 00:19:45 +0300

    Initial Commit

---
+++ Voice Changer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Changer
+//
+//  Created by Viktor Yarmus on 7/24/15.
+//  Copyright (c) 2015 Viktor Yarmus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
