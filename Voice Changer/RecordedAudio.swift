edcccd3bc4dcbf90015701a1dea1103de09b35a7
Viktor Yarmus <mrleomund@gmail.com>
2015-07-25 13:50:42 +0300

    initial commit

---
+++ Voice Changer/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Voice Changer
+//
+//  Created by Viktor Yarmus on 7/24/15.
+//  Copyright (c) 2015 Viktor Yarmus. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
